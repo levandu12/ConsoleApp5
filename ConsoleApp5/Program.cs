84bbbca6d1fa5fb3dbb89e1d01172b6cfa9e066f
levandu12 <146940172+levandu12@users.noreply.github.com>
2024-06-03 14:09:54 +0700

    update name

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApp5
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!, my name is Du");
+            Console.WriteLine("Hello World! duuuuuu");
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
